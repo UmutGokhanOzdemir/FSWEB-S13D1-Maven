e3c418485795c9a139dea46abf35a315fb07f0c8
UmutGokhanOzdemir <umut.gokhan.ozdemir@gmail.com>
2026-05-22 13:38:25 +0300

    Java'ya ve OOP'ye Giriş Gün 1 Projesi Tamamlandı

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,27 +1,45 @@
 package org.example;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) { System.out.println("Hello world!"); }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+        if (clock < 0 || clock > 23) {
+            return false;
+        }
+        if (!isBarking) {
+            return false;
+        }
+        if (clock < 8 || clock >= 20) {
+            return true;
+        }
+        return false;
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+        return isTeen(firstAge) || isTeen(secondAge) || isTeen(thirdAge);
+    }
+
+    public static boolean isTeen(int age) {
+        return age >= 13 && age <= 19;
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+        int upperLimit = isSummer ? 45 : 35;
+        return temp >= 25 && temp <= upperLimit;
     }
 
     public static double area(double width, double height) {
-       
+        if (width < 0 || height < 0) {
+            return -1;
+        }
+        return width * height;
     }
 
     public static double area(double radius) {
-       
+        if (radius < 0) {
+            return -1;
+        }
+        return radius * radius * Math.PI;
     }
 }
